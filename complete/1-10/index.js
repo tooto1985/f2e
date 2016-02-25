6e82dc50eb3f7812bff4983ac2c967f3d0b07bdb
tommy <tooto1985@gmail.com>
2016-02-25 23:42:58 +0800

    test 2

--- complete/1-10/index.js
+++ complete/1-10/index.js
@@ -2,11 +2,11 @@
     $(window).load(function() {
         $(".loading").delay(500).fadeOut();
     });
-    var add = 100 / $("img").length;
     var progress = 0;
     $("img").load(function() {
-        progress += add;
-        $(".txt").text(Math.round(progress) + "%");
-        $(".bar").css("width", progress + "%");
+        progress += 100 / $("img").length;
+        var percent = Math.round(progress) + "%";
+        $(".txt").text(percent);
+        $(".bar").css("width", percent);
     });
 });
