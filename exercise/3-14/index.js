ec8e6e42858ad3e1a3914b26c192b118ba86abfb
Tommy <tooto1985@gmail.com>
2016-02-25 16:05:20 +0800

    dev 5

--- exercise/3-14/index.js
+++ exercise/3-14/index.js
@@ -1,57 +1,57 @@
 var ary = ["A", "B", "C", "D", "E"];
 //length
-console.log(ary.length); //5
+console.log(ary.length);
 //push
-console.log(ary.push("F")); //6
+console.log(ary.push("F"));
 //pop
-console.log(ary.pop()); //"F"
+console.log(ary.pop());
 //unshift
-console.log(ary.unshift("F")); //6
+console.log(ary.unshift("F"));
 //shift
-console.log(ary.shift()); //"F"
+console.log(ary.shift());
 //splice(insert)
-console.log(ary.splice(3, 0, "F")); //[]
+console.log(ary.splice(3, 0, "F"));
 //splice(remove)
-console.log(ary.splice(3, 1)); //["F"]
+console.log(ary.splice(3, 1));
 //indexOf
-console.log(ary.indexOf("D")); //3
+console.log(ary.indexOf("D"));
 //lastIndexOf
-console.log(ary.lastIndexOf("D")); //3
+console.log(ary.lastIndexOf("D"));
 //join
-console.log(ary.join(":")); //"A:B:C:D:E"
+console.log(ary.join(":"));
 //reverse
-console.log(ary.reverse()); //["E", "D", "C", "B", "A"]
+console.log(ary.reverse());
 //sort
-console.log(ary.sort()); //["A", "B", "C", "D", "E"]
+console.log(ary.sort());
 //some
 console.log(ary.some(function(item) {
 	return item === "C";
-})); //true
+}));
 //every
 console.log(ary.every(function(item) {
 	return item.length === 1;
-})); //true
+}));
 //forEach
 console.log(ary.forEach(function(item) {
 	return item;
-})); //undefined
+}));
 //reduce
 console.log(ary.reduce(function(prevItem, item) {
 	return prevItem + item;
-})); //"ABCDE"
+}));
 //reduceRight
 console.log(ary.reduceRight(function(prevItem, item) {
 	return prevItem + item;
-})); //"EDCBA"
+}));
 //map
 console.log(ary.map(function(item) {
 	return item + item;
-})); //["AA", "BB", "CC", "DD", "EE"]
+}));
 //filter
 console.log(ary.filter(function(item) {
 	return item > "C";
-})); //["D", "E"]
+}));
 //slice
-console.log(ary.slice(2, 4)); //["C", "D"]
+console.log(ary.slice(2, 4));
 //concat
-console.log(ary.concat(["G", "H"])); //["A", "B", "C", "D", "E", "G", "H"]
+console.log(ary.concat(["G", "H"]));
