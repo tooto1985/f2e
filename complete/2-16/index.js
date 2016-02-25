0b593904d331d1424ac1401e5f37291f778bbc95
tommy <tooto1985@gmail.com>
2016-02-26 00:01:14 +0800

    test 3

--- complete/2-16/index.js
+++ complete/2-16/index.js
@@ -1,9 +1,9 @@
 ﻿$(function() {
-	var text = "PC版網頁";
+	var text = "PC";
     if (/android/i.test(navigator.userAgent)) {
-    	text = "Android版網頁";
+    	text = "Android";
     } else if (/iphone/i.test(navigator.userAgent)) {
-    	text = "iPhone版網頁";
+    	text = "iPhone";
     }
-    $("body").append("<div>" + text + "</div>");
+    $("body").append("<div>" + text + "版網頁</div>");
 });
