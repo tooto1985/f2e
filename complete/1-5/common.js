35159ac82cc64aef7afa792f76ca74fedf727035
tommy <tooto1985@gmail.com>
2016-02-29 21:07:39 +0800

    test 7

--- complete/1-5/common.js
+++ complete/1-5/common.js
@@ -1,7 +1,8 @@
 ﻿$(function() {
     $(".menu").load("Menu.html", function() {
         var url = location.href;
-        var href = url.substr(url.lastIndexOf("/") + 1);
+        //var href = url.substr(url.lastIndexOf("/") + 1);
+        var href = url.split("/").pop();
         $("a[href='" + (href || "index.html") + "']").addClass("selected");
     });
 });
