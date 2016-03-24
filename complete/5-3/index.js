0709650297444c61c28fb2344b31c9f9da279592
Tommy <tooto1985@gmail.com>
2016-03-24 11:01:32 +0800

    dev 14

--- complete/5-3/index.js
+++ complete/5-3/index.js
@@ -1,19 +1,20 @@
 ﻿$(function() {
     $.ajax({
-        url: "http://json2jsonp.com/?url=http://data.taipei.gov.tw/opendata/apply/json/QUYwMTIzQ0UtNDBEMi00ODA0LTg5RjMtMUQ0QkVCODAzRDIy",
+        url: "//json2jsonp.com/?url=http%3A%2F%2Fdata.taipei%2Fopendata%2Fdatalist%2FapiAccess%3Fscope%3DresourceAquire%26rid%3D42cfc382-f2b8-4c3a-87ad-37249634f78e",
         dataType: "jsonp",
         success: function(data) {
+            var source = data.result.results;
             $("#search").keyup(function() {
                 var val = $(this).val();
                 var html = "";
                 if (val) {
-                    var show = data.filter(function(a) {
+                    var show = source.filter(function(a) {
                         return a.addr.indexOf(val) > -1 || a.name.indexOf(val) > -1;
                     });
                     for (var i = 0; i < show.length; i++) {
                         html += "<div>";
                         html += "<div>" + show[i].name + "</div>";
-                        html += "<div>" + show[i].addr + "</div>";
+                        html += "<a href=\"http://maps.google.com.tw/maps?f=q&hl=zh-TW&z=16&q=" + show[i].addr + "\" target=\"_blank\">" + show[i].addr + "</a>";
                         html += "</div>";
                     }
                 }
