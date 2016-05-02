b057c43f67acea8c10f89a4e9140b45ecb00a243
tommy <tooto1985@gmail.com>
2016-05-02 15:31:20 +0800

    dev 21

---
+++ complete/2-13/index.js
@@ -0,0 +1,6 @@
+﻿$(function() {
+    $(window).scroll(function() {
+        $("#show").text($(this).scrollTop());
+        console.log($(this).scrollTop());
+    }).scroll();
+});
--- complete/2-16/index.js
+++
@@ -1,9 +0,0 @@
-﻿$(function() {
-	var text = "PC";
-    if (/android/i.test(navigator.userAgent)) {
-    	text = "Android";
-    } else if (/iphone/i.test(navigator.userAgent)) {
-    	text = "iPhone";
-    }
-    $("body").append("<div>" + text + "版網頁</div>");
-});
