c1e1a857f37c6eda4819a8379ceb73e8e8cedaaa
Tommy <tooto1985@gmail.com>
2016-05-30 18:05:21 +0800

    dev 24

--- exercise/4-9/index.js
+++ exercise/4-9/index.js
@@ -1,4 +1,24 @@
-﻿//http://tw.somee.com/demo/4-9/data.ashx
-$(function() {
-    
+﻿$(function () {
+    function sendMessage(message) {
+        $.getJSON("http://tw.somee.com/demo/4-9/data.ashx", { name: $("#name").val(), message: message }, function(data) {
+            if (data.length > 0) {
+                for (var i = 0; i < data.length; i++) {
+                    $(".box").append("<div>" + data[i] + "</div>");
+                }
+                $(".box").animate({ "scrollTop": $(".box").prop("scrollHeight") - $(".box").height() }, 200);
+            }
+        });
+        if (message) {
+            $("#message").val("");
+        }
+    }
+    $("#send").click(function() {
+        sendMessage($("#message").val());
+    });
+    $("#message").keyup(function(e) {
+        if (e.keyCode == 13) {
+            sendMessage($(this).val());
+        }
+    });
+    setInterval(sendMessage, 3000);
 });
