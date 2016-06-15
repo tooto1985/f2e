255981baa3477185dfbe77aa31ed12f32522afef
Tommy <tooto1985@gmail.com>
2016-06-15 11:22:35 +0800

    dev 26

--- complete/1-9/index.js
+++ complete/1-9/index.js
@@ -1,6 +1,6 @@
 ﻿$(function() {
 	$("#menu").load("menu.html", function() {
-		$("#menu a").on("mouseenter mouseleave", function() {
+		$(this).find("a").on("mouseenter mouseleave", function() {
 			var temp = $(this).text();
 			$(this).text($(this).data("english"));
 			$(this).data("english", temp);
