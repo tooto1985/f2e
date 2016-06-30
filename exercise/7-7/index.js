8a8c0566d4005c137db0f9da078ca747a28aff54
Tommy <tooto1985@gmail.com>
2016-06-30 15:22:53 +0800

    dev 36

--- exercise/7-7/index.js
+++ exercise/7-7/index.js
@@ -3,15 +3,16 @@
     var $count = $("#count");
     var html = $content.html();
     $("#search").keyup(function() {
-    	var $this = $(this);
-    	var value = $this.val();
-    	var regexp = new RegExp("(?![^<]*>)("+value+")","g");
-    	if (value) {
-    		$count.text(html.match(regexp).length);
-    		$content.html(html.replace(regexp, "<span>$1</span>"));	
-    	} else {
-    		$count.text(0);
-    		$content.html(html);
-    	}
+        var $this = $(this);
+        var value = $this.val().trim();
+        var regexp = new RegExp("(?![^<]*>)(" + value + ")", "igm");
+        var match = html.match(regexp);
+        if (value && match) {
+            $count.text(match.length);
+            $content.html(html.replace(regexp, "<span>$1</span>"));
+        } else {
+            $count.text(0);
+            $content.html(html);
+        }
     });
 });
