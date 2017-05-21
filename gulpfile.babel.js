7c64a2dadfc55e5c82bfd36111a1a39560cb16d7
tommy <tooto1985@gmail.com>
2017-05-21 10:12:04 +0800

    test 27

---
+++ gulpfile.babel.js
@@ -0,0 +1,53 @@
+var gulp = require("gulp");
+var gulpsync = require("gulp-sync");
+var gulpLoadPlugins = require("gulp-load-plugins");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var bom = require("gulp-bom");
+var sourcemaps = require("gulp-sourcemaps");
+var plumber = require("gulp-plumber");
+var debug = require("gulp-debug");
+var chokidar = require('chokidar');
+gulpsync(gulp);
+var $ = gulpLoadPlugins();
+var path = ["complete/**/*.es6.js","exercise/**/*.es6.js"];
+function babel(path) {
+    return gulp.src(path)
+        .pipe(plumber()) // onerror don't stop
+        .pipe(sourcemaps.init()) // sourcemap init
+        .pipe($.babel()) // ES6 to ES5
+        .pipe(uglify()) // minify js
+        .pipe(bom()) // utf-8
+        .pipe(rename(path => {
+            path.basename = path.basename.replace(".es6", "");
+        })) // rename .es6.js to .js
+        .pipe(sourcemaps.write(".")) // sourcemap write
+        .pipe(debug({
+            title: 'debug:'
+        })) // show filename
+        .pipe(gulp.dest(file => {
+            return file.base;
+        })); //output file
+}
+gulp.task("babel", () => {
+    return babel(path);
+});
+gulp.task("default", ["babel","browserSync"]);
+var browserSync = require("browser-sync").create();
+var reload = browserSync.reload;
+gulp.task("browserSync", function() {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+        },
+        port: 80
+    });
+    chokidar.watch("complete/**/*.es6.js").on("all", function(type, file) {
+        babel(file);
+    });
+    chokidar.watch("exercise/**/*.es6.js").on("all", function(type, file) {
+        babel(file);
+    });    
+    gulp.watch("complete/**/*.*").on("change", reload );
+    gulp.watch("exercise/**/*.*").on("change", reload );
+});
--- install.bat
+++ install.bat
@@ -1,2 +1,3 @@
 call npm install
 call npm install gulp -g
+call npm install babel-cli -g
