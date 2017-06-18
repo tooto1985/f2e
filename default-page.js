7b742680a1eff04f9bb3a9ca436ba04d5630ee41
tommy <tooto1985@gmail.com>
2017-06-18 23:50:34 +0800

    dev 60

--- default-page.js
+++ default-page.js
@@ -1,5 +1,6 @@
 
 var fs = require("fs");
+var crypto = require("crypto");
 module.exports = function (req, res, next) {
     if (/^\/(complete|exercise)\/[0-9]+-[0-9]+$/i.test(req.url)) {
         res.writeHead(301, {
@@ -9,10 +10,18 @@ module.exports = function (req, res, next) {
     } else if (/^\/(complete|exercise)\/[0-9]+-[0-9]+\/$/i.test(req.url)) {
         fs.exists("." + req.url + "index.html", function (exists) {
             if (exists) {
+                var data = fs.readFileSync("." + req.url + "index.html");
+                var hash = crypto.createHash('sha1').update(data).digest('base64');
+                if (req.headers['if-none-match'] == hash) {
+                    res.writeHead(304);
+                    res.end();
+                    return;
+                }
                 res.writeHead(200, {
-                    "content-type": "text/html"
+                    "content-type": "text/html",
+                    "Etag":hash
                 });
-                res.write(fs.readFileSync("." + req.url + "index.html"));
+                res.write(data);
                 res.end();
             } else {
                 next();
