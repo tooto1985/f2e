925483fac1a11aabb500f1b87dbfcc68d13c2df7
Tommy <tooto1985@gmail.com>
2017-07-24 15:06:49 +0800

    dev 66

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var defaultPage = require("./default-page");
 var browserSync = require("browser-sync").create();
 var bom = require("gulp-bom");
-var chokidar = require('chokidar');
+var chokidar = require("chokidar");
 var debug = require("gulp-debug");
 var gulp = require("gulp");
 var gulpLoadPlugins = require("gulp-load-plugins");
 var gulpsync = require("gulp-sync");
 var plumber = require("gulp-plumber");
-var proxy = require('proxy-middleware');
+var proxy = require("proxy-middleware");
 var rename = require("gulp-rename");
 var sourcemaps = require("gulp-sourcemaps");
 var uglify = require("gulp-uglify");
-var sass = require('gulp-sass');
-var url = require('url');
+var sass = require("gulp-sass");
+var url = require("url");
 var reload = browserSync.reload;
 var $ = gulpLoadPlugins();
 var folders = ["complete", "exercise"];
@@ -20,7 +20,7 @@ gulpsync(gulp);
 
 function babel(path) {
     return gulp.src(path)
-        .pipe(plumber()) // onerror don't stop
+        .pipe(plumber()) // onerror do not stop
         .pipe(sourcemaps.init()) // sourcemap init
         .pipe($.babel()) // ES6 to ES5
         .pipe(uglify()) // minify js
@@ -30,7 +30,7 @@ function babel(path) {
         })) // rename .es6.js to .js
         .pipe(sourcemaps.write(".")) // sourcemap write
         .pipe(debug({
-            title: 'es6:'
+            title: "es6:"
         })) // show filename
         .pipe(gulp.dest(file => {
             return file.base;
@@ -44,29 +44,29 @@ gulp.task("babel", () => {
 
 function scss(path) {
     return gulp.src(path)
-        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sourcemaps.init())
+        .pipe(sass.sync({ includePaths: ["./"], errLogToConsole: true }).on("error", sass.logError))
+        .pipe(sourcemaps.write("./"))
         .pipe(rename(path => {
             path.basename = path.basename.replace(".cscc", ".css");
         })) // rename .cscc to .css
         .pipe(debug({
-            title: 'scss:'
+            title: "scss:"
         })) // show filename
         .pipe(gulp.dest(file => {
             return file.base;
         }));
 }
 
-gulp.task('sass', function () {
+gulp.task("sass", function () {
     return scss(folders.map(function (a) {
         return a + "/**/*.scss";
     }));
 });
 
-gulp.task("default", ["babel", "sass", "browserSync"]);
-
 gulp.task("browserSync", function () {
-    var proxyOptions = url.parse('http://localhost:3000/api');
-    proxyOptions.route = '/api';
+    var proxyOptions = url.parse("http://localhost:3000/api");
+    proxyOptions.route = "/api";
     browserSync.init({
         server: {
             baseDir: "./",
@@ -75,6 +75,9 @@ gulp.task("browserSync", function () {
         },
         port: 8080
     });
+});
+
+gulp.task("watching", function () {
     folders.forEach(function (a) {
         chokidar.watch(a + "/**/*.es6.js").on("all", function (type, file) {
             babel(file);
@@ -85,3 +88,7 @@ gulp.task("browserSync", function () {
         gulp.watch(a + "/**/*.*").on("change", reload);
     });
 });
+
+gulp.task("default", ["babel", "sass", "browserSync", "watching"]);
+
+gulp.task("noserver", ["babel", "sass", "watching"]);
