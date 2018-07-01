d50cc4907f51adf72008b9fc15852e15ca05dc32
Tommy <tooto1985@gmail.com>
2018-07-01 10:26:40 +0800

    oop 1

--- complete/3-5/index.js
+++ complete/3-5/index.js
@@ -1,60 +1,96 @@
-﻿$(function() {
-    $.getJSON("data.json", function(data) {
-        var html = "";
-        var navi = "";
-        for (var i = 0; i < data.length; i++) {
-            html += "<div>";
-            html += "<a href=\"" + data[i][1] + "\" target=\"_blank\"><img src=\"" + data[i][0] + "\"></a>";
-            html += "</div>";
-            navi += "<span";
-            if (i == 0) {
-                navi += " class=\"active\"";
-            }
-            navi += "></span>";
+﻿var Slide = (function () {
+    function Slide(data) {
+        this.data = data;
+        this.index = 0;
+    }
+    Slide.prototype.next = function () {
+        this.index++;
+        if (this.index >= this.data.length) {
+            this.index = 0;
         }
-        $(".inbox").html(html);
-        $(".navi").html(navi);
-        $(".inbox>div").first().clone().appendTo($(".inbox"));
-        var index = 0;
-        function run() {
+        return this.index;
+    }
+    Slide.prototype.back = function () {
+        this.index--;
+        if (this.index < 0) {
+            this.index = this.data.length - 1;
+        }
+        return this.index;
+    }
+    Slide.prototype.setIndex = function (value) {
+        this.index = value;
+        return this.index;
+    }
+    return Slide;
+})();
+$(function () {
+    function show(data) {
+        var html = "";
+        html += "<div>";
+        html += "<a href=\"" + data[1] + "\" target=\"_blank\"><img src=\"" + data[0] + "\"></a>";
+        html += "</div>";
+        return html;
+    }
+    function dot(index) {
+        $(".navi>span.active").removeClass();
+        $(".navi>span").eq(index).addClass("active");
+    }
+    $.getJSON("data.json", function (data) {
+        var slide = new Slide(data);
+        $(".next").click(function () {
             if (!$(".inbox").is(":animated")) {
-                $(".inbox").animate({left: "-=800"}, function() {
-                    if (index >= data.length - 1) {
-                        index = -1;
+                slide.next();
+                $(".inbox").animate({ left: "-=800" }, function () {
+                    if (slide.index === 0) {
                         $(this).css("left", 0);
                     }
-                    index++;
-                    dot();
+                    dot(slide.index);
                 });
             }
-        }
-        var sid = setInterval(run, 2000);
-        $(".inbox,.navi,.prev,.next").hover(function() {
-            clearInterval(sid);
-        },function() {
-            sid = setInterval(run, 2000);
         });
-        function dot() {
-            $(".navi>span.active").removeClass();
-            $(".navi>span").eq(index).addClass("active");
-        }
-        function back() {
+        $(".prev").click(function () {
             if (!$(".inbox").is(":animated")) {
-                if (index <= 0) {
-                    index = data.length;
-                    $(".inbox").css("left", index * -800);
+                if (slide.index === 0) {
+                    $(".inbox").css("left", slide.data.length * -800);
                 }
-                $(".inbox").animate({left: "+=800"}, function() {
-                    index--;
-                    dot();
+                slide.back();
+                $(".inbox").animate({ left: "+=800" }, function () {
+                    dot(slide.index);
                 });
             }
-        }
-        $(".next").click(run);
-        $(".prev").click(back);
-        $(".navi>span").click(function() {
-            index = $(this).index();
-            $(".inbox").animate({left: index * -800}, dot);
         });
+        $(".navi").on("click", ">span" ,function () {
+            if (!$(".inbox").is(":animated")) {
+                $(".inbox").animate({ left: slide.setIndex($(this).index()) * -800 }, function () {
+                    dot(slide.index);
+                });
+            }
+        });
+        (function () {
+            for (var i = 0; i <= slide.data.length; i++) {
+                var index = i;
+                if (i === slide.data.length) {
+                    index = 0;
+                }
+                $(".inbox").append(show(slide.data[index]));
+                if (i < slide.data.length) {
+                    var $span = $("<span>");
+                    if (i === 0) {
+                        $span.addClass("active");
+                    }
+                    $(".navi").append($span);
+                }
+            }
+            var sid = setInterval(function() {
+                $(".next").click()
+            }, 3000);
+            $(".inbox,.prev,.next,.navi").hover(function() {
+                clearInterval(sid);
+            }, function() {
+                sid = setInterval(function() {
+                    $(".next").click()
+                }, 3000);
+            })
+        })(); //init
     });
 });
