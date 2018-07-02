b033ab36058ad3c743daaecf20d7538b2268e139
Tommy <tooto1985@gmail.com>
2018-07-02 19:42:17 +0800

    dev 82

--- complete/7-4/index.js
+++ complete/7-4/index.js
@@ -1,7 +1,7 @@
 ﻿$(function() {
-    var letter = "abcdefghjklmnpqrstuvxywzio";
-    var weight = "19876543211";
     function check(cid) {
+        var letter = "abcdefghjklmnpqrstuvxywzio";
+        var weight = "19876543211";        
         cid = cid.toLowerCase();
         cid = letter.indexOf(cid[0]) + 10 + cid.substr(1);
         return cid.split("").reduce(function(prev, next, i) {
