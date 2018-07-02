61b11d4128becc0611f47a24b9262c418bed1096
Tommy <tooto1985@gmail.com>
2018-07-02 19:39:39 +0800

    dev 81

--- complete/7-4/index.js
+++ complete/7-4/index.js
@@ -1,6 +1,6 @@
 ﻿$(function() {
-    var letter = ["a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "u", "v", "x", "y", "w", "z", "i", "o"];
-    var weight = [1, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1];
+    var letter = "abcdefghjklmnpqrstuvxywzio";
+    var weight = "19876543211";
     function check(cid) {
         cid = cid.toLowerCase();
         cid = letter.indexOf(cid[0]) + 10 + cid.substr(1);
