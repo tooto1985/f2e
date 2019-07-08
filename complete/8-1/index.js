3e889c8083df5269f106d0f92ecc56e9b0cca198
Tommy <tooto1985@gmail.com>
2019-07-08 16:05:26 +0800

    temp 8

---
+++ complete/8-1/index.js
@@ -0,0 +1,24 @@
+﻿new Vue({
+    el: "#app",
+    data: function() {
+        return {
+            searchText: '',
+            userList: []
+        }
+    },
+    computed: {
+        userListInSearch: function() {
+            var regexp = new RegExp(this.searchText,"i");
+            return this.userList.filter(function(a) {
+                return regexp.test(a.name);
+            }.bind(this));
+        }
+    },
+    created: function() {
+        fetch("data.json").then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            this.userList = data;
+        }.bind(this));
+    }
+});
---
+++ complete/8-2/index.js
@@ -0,0 +1,42 @@
+﻿new Vue({
+    el: "#app",
+    data: function() {
+        return {
+            classList: [],
+            sortKey: "",
+            sortDirection: ""
+        };
+    },
+    created: function() {
+        fetch("data.json").then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            this.classList = data;
+        }.bind(this));
+    },
+    methods: {
+        sortBy: function(key) {
+            if (key === this.sortKey) {
+                this.sortDirection = this.sortDirection === "up" ? "down" : "up";
+            } else {
+                this.sortKey = key;
+                this.sortDirection = "up";
+            }
+            this.classList.sort(function(a, b) {
+                if (this.sortDirection == "up") {
+                    if (a[this.sortKey] > b[this.sortKey]) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                } else {
+                    if (a[this.sortKey] < b[this.sortKey]) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                }
+            }.bind(this));
+        }
+    }
+});
