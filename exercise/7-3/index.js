34c24ec953548efb08b05244171bffa2c6394bf4
Tommy <tooto1985@gmail.com>
2019-08-11 20:17:48 +0800

    dev 95

--- exercise/7-3/index.js
+++ exercise/7-3/index.js
@@ -2,9 +2,7 @@
     el: "#app",
     data: function() {
         return {
-            newsList: [],
-            pagerCount: 5,
-            currentIndex: 1
+            newsList: []
         };
     },
     created: function() {
@@ -13,37 +11,5 @@
         }).then(function(data) {
             this.newsList = data;
         }.bind(this));
-    },
-    computed: {
-        pager: function() {
-            return this.newsList.filter(function(a) {
-                var i = this.newsList.indexOf(a);
-                return i >= this.pagerCount * (this.currentIndex - 1) && i < this.pagerCount * this.currentIndex;
-            }.bind(this));
-        },
-        maxPagerIndex: function() {
-            return Math.ceil(this.newsList.length / this.pagerCount);
-        }
-    },
-    methods: {
-        moveTo: function(value) {
-            if (value === "first") {
-                this.currentIndex = 1;
-            } else if (value === "prev") {
-                this.currentIndex--;
-                if (this.currentIndex < 1) {
-                    this.currentIndex = 1;
-                    alert("沒有上一頁了");
-                }
-            } else if (value === "next") {
-                this.currentIndex++;
-                if (this.currentIndex > this.maxPagerIndex) {
-                    this.currentIndex = this.maxPagerIndex;
-                    alert("沒有下一頁了");
-                }
-            } else if (value === "last") {
-                this.currentIndex = this.maxPagerIndex;
-            }
-        }
     }
 });
