03da0441c6f495655a60abc9b7f0ae4fc6caa2f5
Tommy <tooto1985@gmail.com>
2019-11-13 21:48:20 +0800

    self 1

---
+++ complete/3-13/common.js
@@ -0,0 +1,28 @@
+﻿$(function() {
+    function build(data, parent) {
+        var html = "";
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].parent == parent) {
+                html += "<li>";
+                html += "<a href=\"" + data[i].url + "\">" + data[i].name + "</a>";
+                html += "<ul>" + build(data, data[i].id) + "</ul>";
+                html += "</li>";
+            }
+        }
+        return html;
+    }
+    $.getJSON("data.json", function(data) {
+        $(".navigation").html(build(data, 0));
+    });
+    $(".navigation").on("mouseenter", "a", function() {
+        var $parent = $(this).parent();
+        var link = "";
+        while (!$parent.hasClass("navigation")) {
+            if ($parent.is("li")) {
+                link = ">" + $parent.find(">a").text() + link;
+            }
+            $parent = $parent.parent();
+        }
+        $(".link").html(link.substr(1));
+    });
+});
